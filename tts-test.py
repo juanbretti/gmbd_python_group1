c49f2938c5e0575905925c7a46f8b3b820ad736a
Juan Pedro Bretti Mandarano <juan+github@bretti.com.ar>
2020-09-13 17:38:37 +0200

    Changed the type of rbaValue

--- tts-test.py
+++ tts-test.py
@@ -37,10 +37,17 @@ engine.runAndWait()
 from gtts import gTTS
 import os 
 
-tts = gTTS('My current speaking rate is, this is a demo using TTS. Please consider saying Juan Pedro.')
-tts.save('hello.mp3')
+# tts = gTTS('There is a particular case in our dataset, that the column “birthplace”. This column has missing values in another way. We addressed this column in a particular way.')
+tts = gTTS('Let’s do a quick exploratory description of the column.\
+    Using “value_counts()” we get in descending order so that the first element is the most frequently-occurring element.\
+    It’s evident that the cells that contain “?” are not valid inputs in our data.\
+    To fix it, we did a for loop that is controlled with the .find().')
+# tts = gTTS('There is a particular case in our dataset, that the column “birthplace”. This column has missing values in another way. We addressed this column in a particular way.')
+# tts = gTTS('There is a particular case in our dataset, that the column “birthplace”. This column has missing values in another way. We addressed this column in a particular way.')
+# tts = gTTS('There is a particular case in our dataset, that the column “birthplace”. This column has missing values in another way. We addressed this column in a particular way.')
+tts.save('jp_slide_3.mp3')
 
-os.system("mpg321 hello.mp3") 
+# os.system("mpg321 hello.mp3") 
 # %%
 # https://stackoverflow.com/questions/58614450/is-there-a-module-that-allows-me-to-make-python-say-things-as-audio-through-the
 # https://stackoverflow.com/questions/1614059/how-to-make-python-speak
